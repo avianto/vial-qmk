b2890aff4c40ab11a8cd9fdc5c361a1e41049f88
avianto <dev@avianto.com>
2021-09-14 17:30:50 -0400

    Add Vial support for UT47.2

---
+++ keyboards/ut472/keymaps/vial/config.h
@@ -0,0 +1,16 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x59, 0x67, 0x22, 0x6A, 0x19, 0x87, 0x01, 0x17}
+
+
+#define VIAL_UNLOCK_COMBO_ROWS { 1,1 }
+#define VIAL_UNLOCK_COMBO_COLS { 0,11 }
+
+#ifdef RGB_DI_PIN
+#undef RGBLIGHT_ANIMATIONS
+#define RGBLIGHT_EFFECT_BREATHING
+#define RGBLIGHT_EFFECT_RAINBOW_MOOD
+#define RGBLIGHT_EFFECT_RAINBOW_SWIRL
+#endif
