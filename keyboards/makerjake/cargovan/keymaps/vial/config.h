50978d9e93e1658ac7c4a9730646f830b3790c15
avianto <dev@avianto.com>
2021-12-06 09:47:48 -0500

    Add Cargovan support

---
+++ keyboards/makerjake/cargovan/keymaps/vial/config.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x9F, 0x68, 0xB8, 0x44, 0x32, 0xF7, 0xB9, 0x0B}
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 0 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 10 }
