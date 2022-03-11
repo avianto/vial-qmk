d7b8bebeb26dd1c17bb73438c8673cc03b5f37b7
avianto <dev@avianto.com>
2022-03-10 22:43:26 -0500

    Add 3999 Ortho

---
+++ keyboards/3999_ortho/keymaps/vial/config.h
@@ -0,0 +1,27 @@
+/* Copyright 2021 dpalka15 / avianto
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#pragma once
+
+#define IGNORE_MOD_TAP_INTERRUPT //helps with homerow mods
+#define COMBO_TERM 30
+#define TAPPING_TERM 200
+
+/* VIAL Specific definitions */
+#define VIAL_KEYBOARD_UID {0x3C, 0xF4, 0xB1, 0x4C, 0xC2, 0x64, 0x92, 0xA9}
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 0 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 9 }
+#define VIAL_COMBO_ENTRIES 10
+// #define VIAL_ENCODER_DEFAULT { KC_VOLU, KC_VOLD, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS }
---
+++ keyboards/3999_ortho/keymaps/vial/rules.mk
@@ -0,0 +1,6 @@
+COMBO_ENABLE 			= yes
+# VIAL_ENCODERS_ENABLE 	= yes
+VIA_ENABLE 				= yes
+VIAL_ENABLE 			= yes
+
+RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
