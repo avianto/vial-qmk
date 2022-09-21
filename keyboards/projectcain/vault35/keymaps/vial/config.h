5b973504687f944abd1ffef239b34c7f5440da3a
avianto <dev@avianto.com>
2022-09-21 01:17:17 -0400

    vault35 config

--- keyboards/projectcain/vault35/keymaps/vial/config.h
+++ keyboards/projectcain/vault35/keymaps/vial/config.h
@@ -14,7 +14,7 @@
 #ifdef RGBLIGHT_ENABLE
   #define RGB_DI_PIN D5
     #ifdef RGB_DI_PIN
-    #define RGBLED_NUM 6
+    #define RGBLED_NUM 12
     #define RGBLIGHT_HUE_STEP 8
     #define RGBLIGHT_SAT_STEP 8
     #define RGBLIGHT_VAL_STEP 8
